253384fdf4ab4b8115b7d5961a130003ddcd1fc6
suronjit02 <suronjit02@gmail.com>
2025-08-15 18:21:51 +0600

    4th assignment ready for sumbit, fixe all

--- Assignment-4/final.js
+++ Assignment-4/final.js
@@ -1,5 +1,6 @@
 
 
+
 function totalFine(fare) {
     // You have to write your code here
 
---
+++ Assignment-4/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+[]
+// { finalScore: 0, pass: 0, fail: 0 }
+
+// [98, 87, 67, 91, 92, 33, 87]
+// { finalScore: 79, pass: 6, fail: 1 }
+
+// [99, 87, 67, 12, 87]
+// { finalScore: 70, pass: 4, fail: 1 }
+
+// [99]
+// { finalScore: 99, pass: 1, fail: 0 }
+
+// 100
+// Invalid
